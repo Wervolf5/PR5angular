daed35014daa0362917250ec46577384b7013fea
Wervolf5 <maksimmelnicow@mail.ru>
2023-01-16 01:36:49 +0100

    prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, HostListener} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -13,77 +14,48 @@ export class AppComponent {
     car: ['', Validators.required],
   });
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "полный",
-      engine: 6.2,
-      places: 2
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "полный",
-      engine: 3.9,
-      places: 4
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "полный",
-      engine: 4.0,
-      places: 5
-    },
-    {
-      image: "5.png",
-      name: "Lamborghini Urus",
-      gear: "полный",
-      engine: 5.2,
-      places: 2
-    },
-    {
-      image: "6.png",
-      name: "Chevrolet Camaro",
-      gear: "полный",
-      engine: 2.0,
-      places: 4
-    },
-  ];
+  carsData: any;
 
-  constructor(private fb: FormBuilder) {
-
+  constructor(private fb: FormBuilder, private appService: AppService) {
   }
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carsData => this.carsData = carsData);
+  }
+
   goScroll(target: HTMLElement, car?: any) {
-    target.scrollIntoView({behavior: "smooth"});
+    target.scrollIntoView({ behavior: "smooth" });
     if (car) {
-      this.priceForm.patchValue({car: car.name});
+      this.priceForm.patchValue({ car: car.name });
     }
   }
 
   trans: any;
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
-    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+    this.trans = { transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)' };
   }
-  
+
   bgPos: any;
   @HostListener('document:scroll', ['$event'])
   onScroll() {
-    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+    this.bgPos = { backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px' };
   }
 
   onSubmit() {
     if (this.priceForm.valid) {
-      alert("Спасибо за заявку,мы свяжемся с вами в ближайшее время!")
-      this.priceForm.reset();
+      this.appService.sendQuery(this.priceForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              alert(response.message)
+              this.priceForm.reset();
+            },
+            error: (response) => {
+              alert(response.error.message);
+            }
+          }
+        );
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http"
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
